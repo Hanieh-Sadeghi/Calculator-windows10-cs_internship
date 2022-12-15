fc013b70703c9812073b725fb06631faa2b489d5
banfshesadeghi <haniehsadeghi2@uotlook.com>
2022-12-15 23:57:25 +0330

    add js

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,5 +1,8 @@
 const numbers = document.querySelectorAll('.keyboardCalculator');
 let test = '';
+const deleteUnit = document.getElementById('.delete');
+const deleteCE = document.getElementById('.CE');
+const deleteAll = document.getElementById('.C');
 
 // for(let i=0; i<numbers.length; i++){
 //      numbers[i].addEventListener('click',( e)=> {
@@ -19,11 +22,12 @@ numbers.forEach ((buttons) => {
 
 function display (numbers) {
      const display = document.getElementById ('Zero')
-     display.innerHTML = numbers
-     
+     display.innerHTML = numbers;
 }
 
 function clearDisplay () {
      const display = document.getElementById ('')
      display.innerHTML = ''   
 }
+
+deleteAllNumber .addEventListener()
