40e48452624c6471759dc7bbd05afef8da9b2c88
banfshesadeghi <haniehsadeghi2@uotlook.com>
2022-12-29 19:44:04 +0330

    Master add JS

--- Calculator/Theme/JS/app.js
+++ Calculator/Theme/JS/app.js
@@ -1,65 +1,72 @@
-const numbers = document.querySelectorAll('.keyboardCalculator');
-let temp = ''
-let number1 = '';
-let number2 = '';
-let sign = '';
-const deleteUnit = document.getElementById('.delete');
-const deleteCE = document.getElementById('.CE');
-const deleteAll = document.getElementById('.C');
+const numbers = document.querySelectorAll(".keyboardCalculator");
+let temp = "";
+let number1 = "";
+let number2 = "";
+let sign = "";
+const deleteUnit = document.getElementById(".delete");
+const deleteCE = document.getElementById(".CE");
+const deleteAll = document.getElementById(".C");
 
-numbers.forEach ((buttons) => {
-     buttons.addEventListener('click', (e) => {
-          e.preventDefault (); 
-          let value = e.target.getAttribute ( 'value' ) 
-          temp = temp + value;
-          display(temp) 
-          if( value == 'C'){
-               display("0")
-          }else if( value == 'backspace'){
-               display("-1")
-          } else if (value == 'CE'){
-               display("0")
-          }
-          let signs  =  '+-×÷'
-        
-         if(signs.includes(value)){ 
-              number1 = temp;
-              display(number1)
-              temp = '';
-              sign = signs;
-             console.log(number1)
+numbers.forEach((buttons) => {
+  buttons.addEventListener("click", (e) => {
+    e.preventDefault();
+    let signs = "+-×÷";
 
-     } else if(value== '=') {
-          number2 = temp;
-          display(number2)
-          temp = '';
-          console.log(number2)
-          let result = Operation(number1, number2, signs );
-          console.log(result)
-     }
-     
-  })  
-})
+    let value = e.target.getAttribute("value");
 
-function display (numbers) {
-     const display = document.getElementById ('Zero')
-     display.innerHTML = numbers;
+    if (signs.includes(value)) {
+      sign = value;
+      value = "";
+      number1 = Number(temp);
+      temp = "";
+    } else if (value == "=") {
+      number2 = Number(temp);
+      temp = "";
+      let result = calculate(number1, number2, sign);
+      display("");
+      display(result);
+    } else {
+      temp = temp + value;
+      display(temp);
+    }
+
+    if (value == "C") {
+      display("0");
+    } else if (value == "backspace") {
+      display("-1");
+    } else if (value == "CE") {
+      display("0");
+    }
+  });
+});
+
+function display(numbers) {
+  const display = document.getElementById("Zero");
+  display.innerHTML = numbers;
 }
 
-function clearDisplay () {
-     const display = document.getElementById ('.deleteUnit')
-     display.innerHTML = ' '   ;
+function clearDisplay() {
+  const display = document.getElementById(".deleteUnit");
+  display.innerHTML = " ";
 }
 
-function Operation(firstNumber, secondNumber, operand) {
-     result = 0 ;
-     // switch (operand){
-     //      case '+' :     result = firstNumber + secondNumber  ;  break;
-     //      case '-' :      result = firstNumber -  secondNumber ;  break;
-     //      case '*' :      result = firstNumber * secondNumber ; break;
-     //      case '/' :      result = firstNumber /  secondNumber ;  break;
-     // }
-     result = firstNumber + secondNumber
+let result = "";
+function calculate(firstNumber, secondNumber, operand) {
+  result = 0;
+  switch (operand) {
+    case "+":
+      result = firstNumber + secondNumber;
+      break;
+    case "-":
+      result = firstNumber - secondNumber;
+      break;
+    case "×":
+      result = firstNumber * secondNumber;
+      break;
+    case "÷":
+      result = firstNumber / secondNumber;
+      break;
+  }
 
-     return result;
+  return result;
 }
