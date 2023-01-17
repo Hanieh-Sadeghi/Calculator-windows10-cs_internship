79e730a605d2f85a8de8956818cff5e3e784c22c
banfshesadeghi <haniehsadeghi2@uotlook.com>
2023-01-17 20:54:17 +0330

    Add the function

--- Calculator/Theme/JS/History.js
+++ Calculator/Theme/JS/History.js
@@ -1,4 +1,10 @@
-// debugger;
 const memoryBtn = document.querySelector (".mline")
 const mc = document.querySelector(".mc ");
 const mr = document.querySelector(".mr");
+
+
+let HistoryLog = [ ];
+function addHistory(result){
+
+
+}
