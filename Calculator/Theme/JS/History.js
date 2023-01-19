de02274e24a85ddd980dfc10d925208b396ae42d
banfshesadeghi <haniehsadeghi2@uotlook.com>
2023-01-19 22:32:42 +0330

    Add variables const && let ,,,history && add trash

--- Calculator/Theme/JS/History.js
+++ Calculator/Theme/JS/History.js
@@ -1,5 +1,16 @@
+const history = document.getElementById('history-memory-p1');
+const memory = document.getElementById('history-memory-p2');
+const list = document.getElementById('history-msg-list');
+const trash = document.getElementById('material-symbols-outlined');
+const eventHandler = {};
+let state = 'history';
 
-export let HistoryLog = [ ];
-export function addHistory(result){
-HistoryLog.push(result);
+export let HistoryLog = [];
+export function addHistory(result) {
+    HistoryLog.push(result);
 }
+
+trash.addEventListener('click', (e) => {
+    e.preventDefault();
+    eventHandler.trash(state);
+})
