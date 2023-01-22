71c0ef1dffe4ebda33b8cb5a3866fc28e6a7dbf9
banfshesadeghi <haniehsadeghi2@uotlook.com>
2023-01-22 20:33:35 +0330

    add

--- Calculator/Theme/JS/History.js
+++ Calculator/Theme/JS/History.js
@@ -1,5 +1,5 @@
-const history = document.getElementById('history-memory-p1');
-const memory = document.getElementById('history-memory-p2');
+const history = document.getElementById('history-p1');
+const memory = document.getElementById('memory-p2');
 const list = document.getElementById('history-msg-list');
 const trash = document.getElementById('material-symbols-outlined');
 const eventHandler = {};
@@ -10,7 +10,7 @@ export function addHistory(result) {
     HistoryLog.push(result);
 }
 
-trash.addEventListener('click', (e) => {
-    e.preventDefault();
-    eventHandler.trash(state);
-})
+// trash.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     eventHandler.trash(state);
+// })
