cf28591787cb0511ad9ee28f36266ac5529dce9f
banfshesadeghi <haniehsadeghi2@uotlook.com>
2023-01-31 21:15:23 +0330

    add displayTempBox

--- Calculator/Theme/JS/app.js
+++ Calculator/Theme/JS/app.js
@@ -4,20 +4,19 @@ let number1 = "";
 let number2 = "";
 let sign = "";
 let result = "";
+let numberValue = document.querySelector("#Zero");
+let boxResult = document.querySelector('.boxResult');
+let historyDiv = document.getElementById("historyLog");
 const numbers = document.querySelectorAll(".keyboardCalculator");
-const backSpace = document.getElementById(".backSpace");
 const deleteCE = document.getElementById(".CE");
 const deleteAll = document.getElementById(".C");
-let historyDiv = document.getElementById("historyLog");
 const trash = document.getElementById('material-symbols-outlined');
-
 
 numbers.forEach((buttons) => {
   buttons.addEventListener("click", (e) => {
     e.preventDefault();
-    let signs = "+-×÷";
+    let signs = '+-×÷';
     let topSigns = ["X2", "X3", "√", "1/X"];
-
     let value = e.target.getAttribute("value");
 
     if (signs.includes(value)) {
@@ -25,7 +24,8 @@ numbers.forEach((buttons) => {
       value = "";
       number1 = Number(temp);
       temp = "";
-    } else if (value == "=") {
+      displayTempBox(number1, sign);
+    } else if (value == '=') {
       number2 = Number(temp);
       temp = "";
       let result = calculate(number1, number2, sign);
@@ -37,15 +37,18 @@ numbers.forEach((buttons) => {
         console.log(historyModule.HistoryLog);
         historyDiv.innerHTML += `<div class="historyNumber"> ${historyModule.HistoryLog[i]}</div>`;
       }
+      displayTempBox(number1, sign, number2, value);
     } else if (topSigns.includes(value)) {
       let currentValue = parseInt(numberValue.innerHTML);
       display(exponent_calculate(currentValue, value));
-    } else if (value == "%") {
+      displayTempBox(currentValue, value);
+    } else if (value == '%') {
       let per = 2
       let temp2 = Number(temp)
       let result = percentage(temp2, per);
       display(result)
-    } else if (value == "backspace") {
+      displayTempBox(temp2, value);
+    } else if (value == 'backspace') {
       temp = temp.substring(0, temp.length - 1);
       if (temp == "") {
         display(0)
@@ -56,13 +59,10 @@ numbers.forEach((buttons) => {
       temp = temp + value;
       display(temp);
     }
-
-    if (value == "C") {
-      clearDisplay("deleteAll")
-
-    } else if (value == "CE") {
-      display("0");
-
+    if (value == 'C') {
+      clearDisplay('deleteAll');
+    } else if (value == 'CE') {
+      display('0');
     }
   });
 });
@@ -92,10 +92,21 @@ function display(numbers) {
   display.innerHTML = numbers;
 }
 
-function displayTempBox(numbers, signs) {
-  let displayBox = `${numbers} ${signs}`;
-  displayBox.innerHTML = `${numbers} ${signs}`;
+function displayTempBox() {
+  switch (arguments.length) {
+    case 2: {
+      let displayBox = `${arguments[0]} ${arguments[1]}`; //backtick
+      boxResult.innerHTML = displayBox;
+      break;
+    }
+    case 4: {
+      let displayBox = `${arguments[0]}  ${arguments[1]} ${arguments[2]} ${arguments[3]}`;
+      boxResult.innerHTML = displayBox;
+      break;
+    }
+  }
 }
+
 
 function calculate(firstNumber, secondNumber, operand) {
   result = 0;
