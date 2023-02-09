8100808e359ce6e31407ddbd89a2c9c4a1c1f7d1
Banasheh Sadeghi <haniehsadeghi2@outlook.com>
2023-02-09 14:28:55 +0330

    add displayTempBox, add negative ,change '' ,change backtick

--- Calculator/Theme/JS/app.js
+++ Calculator/Theme/JS/app.js
@@ -1,70 +1,89 @@
 import *  as historyModule from './History.js'
 
-let temp = "";
-let number1 = "";
-let number2 = "";
-let sign = "";
-let result = "";
-let numberValue = document.querySelector("#Zero");
+let temp = '';
+let number1 = '';
+let number2 = '';
+let sign = '';
+let result = '';
+let numberValue = document.querySelector('#Zero');
 let boxResult = document.querySelector('.boxResult');
-let historyDiv = document.getElementById("historyLog");
-const numbers = document.querySelectorAll(".keyboardCalculator");
-const deleteCE = document.getElementById(".CE");
-const deleteAll = document.getElementById(".C");
+let historyDiv = document.getElementById('historyLog');
+const numbers = document.querySelectorAll('.keyboardCalculator');
+const deleteCE = document.getElementById('.CE');
+const deleteAll = document.getElementById('.C');
 const trash = document.getElementById('material-symbols-outlined');
+let numberList = [];
+let signList = [];
+let historyLog = [];
 
 numbers.forEach((buttons) => {
-  buttons.addEventListener("click", (e) => {
+  buttons.addEventListener('click', (e) => {
     e.preventDefault();
     let signs = '+-×÷';
-    let topSigns = ["X2", "X3", "√", "1/X"];
-    let value = e.target.getAttribute("value");
+    let topSigns = ['X2', 'X3', '√', '1/X'];
+    let value = e.target.getAttribute('value');
+    let changeSign = '+/-';
 
     if (signs.includes(value)) {
       sign = value;
-      value = "";
+      value = '';
       number1 = Number(temp);
-      temp = "";
-      displayTempBox(number1, sign);
-      historyModule.historyTempBox(number1, sign);
+      temp = '';
+      numberList.push(number1);
+      signList.push(sign);
+      displayTempBox();
+
     } else if (value == '=') {
       number2 = Number(temp);
-      temp = "";
-      let result = calculate(number1, number2, sign);
-      display("");
+      numberList.push(number2);
+      signList.push(value);
+      let result = calculate();
+      temp = result;
+      display(' ');
       display(result);
+      displayTempBox();
       historyModule.addHistory(result);
-      historyDiv.innerHTML = "";
+      historyDiv.innerHTML = '';
+      historyLog.push(boxResult.innerHTML);
       for (let i = 0; i < historyModule.HistoryLog.length; i++) {
-        console.log(historyModule.HistoryLog);
-        historyDiv.innerHTML += `<div class="historyNumber"> ${historyModule.HistoryLog[i]}</div>`;
+        historyDiv.innerHTML += `<div class='historyTemp'>${historyLog[i]}</div> <div class='historyNumber'> ${historyModule.HistoryLog[i]}</div>`;
       }
-      displayTempBox(number1, sign, number2, value);
-      historyModule.historyTempBox(number1, sign, number2, value);
+      numberList = [];
+      signList = [];
+      historyModule.historyTempBox();
+
     } else if (topSigns.includes(value)) {
       let currentValue = parseInt(numberValue.innerHTML);
+      numberList.push(currentValue);
+      signList.push(value);
       display(exponent_calculate(currentValue, value));
-      historyModule.historyTempBox(currentValue, value);
+      displayTempBox()
+      historyModule.historyTempBox();
+
     } else if (value == '%') {
-      let per = 2
-      let temp2 = Number(temp)
-      let result = percentage(temp2, per);
-      display(result)
-      displayTempBox(temp2, value);
-      historyModule.historyTempBox(temp2, value);
+      let per = temp;
+      let result = percentage(number1, per);
+      numberList.push(Number(per));
+      signList.push(value);
+      display(number1 - result);
+      displayTempBox();
 
     } else if (value == 'backspace') {
       temp = temp.substring(0, temp.length - 1);
-      if (temp == "") {
+      if (temp == '') {
         display(0)
       } else {
         display(temp);
       }
+    } else if (value == changeSign) {
+      negative(Number(numberValue.innerHTML));
+      display(numberValue.innerHTML);
     } else {
       if (value === '.' && temp.includes('.')) return;
       temp = temp + value;
       display(temp);
     }
+
     if (value == 'C') {
       clearDisplay('deleteAll');
       clearTempBox();
@@ -78,18 +97,18 @@ numbers.forEach((buttons) => {
 
 function clearDisplay(type) {
   switch (type) {
-    case "deleteAll":
-      number1 = "";
-      number2 = "";
-      temp = "";
+    case 'deleteAll':
+      number1 = '';
+      number2 = '';
+      temp = '';
       display(0);
       break;
 
-    case "deleteCE":
+    case 'deleteCE':
       display(0);
       temp = '';
       console.log(temp)
-      number2 = "";
+      number2 = '';
       break;
   }
 }
@@ -99,61 +118,56 @@ function clearTempBox() {
 }
 
 function display(numbers) {
-  const display = document.getElementById("Zero");
+  const display = document.getElementById('Zero');
   display.innerHTML = numbers;
 }
 
 function displayTempBox() {
-  switch (arguments.length) {
-    case 2: {
-      let displayBox = `${arguments[0]} ${arguments[1]}`; //backtick
-      boxResult.innerHTML = displayBox;
-      break;
-    }
-    case 4: {
-      let displayBox = `${arguments[0]}  ${arguments[1]} ${arguments[2]} ${arguments[3]}`;
-      boxResult.innerHTML = displayBox;
-      break;
-    }
+  let displayBox = '';
+  for (let i = 0; i < numberList.length; i++) {
+    displayBox += `${numberList[i]} ${signList[i]}`;
   }
+  boxResult.innerHTML = displayBox;
 }
 
-function calculate(firstNumber, secondNumber, operand) {
-  result = 0;
-  switch (operand) {
-    case "+":
-      result = firstNumber + secondNumber;
-      break;
-    case "-":
-      result = firstNumber - secondNumber;
-      break;
-    case "×":
-      result = firstNumber * secondNumber;
-      break;
-    case "÷":
-      result = firstNumber / secondNumber;
-      break;
+function calculate() {
+  result = numberList[0];
+  for (let i = 0; i < numberList.length - 1; i++) {
+    let currentSign = signList[i];
+    switch (currentSign) {
+      case '+':
+        result += numberList[i + 1];
+        break;
+      case '-':
+        result -= numberList[i + 1];
+        break;
+      case '×':
+        result *= numberList[i + 1];
+        break;
+      case '÷':
+        result /= numberList[i + 1];
+        break;
+    }
   }
-
   return result;
 }
 
 function exponent_calculate(number, operator) {
   let result = 0;
   switch (operator) {
-    case "√": {
+    case '√': {
       result = Math.sqrt(number);
       break;
     }
-    case "X2": {
+    case 'X2': {
       result = Math.pow(number, 2);
       break;
     }
-    case "X3": {
+    case 'X3': {
       result = Math.pow(number, 3);
       break;
     }
-    case "1/X": {
+    case '1/X': {
       result = number = 1 / number;
       break;
     }
@@ -162,10 +176,23 @@ function exponent_calculate(number, operator) {
 }
 
 function percentage(number, per) {
-  return (number / 100) * per;
+  return (number * per) / 100;
 }
 
 trash.addEventListener('click', (e) => {
   e.preventDefault();
   historyDiv.innerHTML = '';
 })
+
+function negative(number) {
+  numberValue.innerHTML = number * -1;
+  temp = numberValue.innerHTML;
+}
+
+// read array in javascript
+
+function readArrayNumber() {
+  for (let i = 0; i < numberList.length; i++) {
+    console.log(numberList[i]);
+  }
+}
