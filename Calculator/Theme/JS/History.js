21cde197b7978345886a59d9478170aad3f86c39
Banasheh Sadeghi <haniehsadeghi2@outlook.com>
2023-02-09 14:32:18 +0330

    add function historyTempBox

--- Calculator/Theme/JS/History.js
+++ Calculator/Theme/JS/History.js
@@ -1,24 +1,16 @@
-const history = document.getElementById('history-p1'); //History
+const history = document.getElementById('history-1'); //History
 const memory = document.getElementById('memory-p2');//Memory
 const list = document.getElementById('history-msg'); //list history&&Memory
-let historyNumber =document.getElementsByClassName('historyNumber');
+let historyNumber = document.getElementsByClassName('historyNumber');
 
 export let HistoryLog = [];
 export function addHistory(result) {
-    HistoryLog.push(result);
+  HistoryLog.push(result);
 }
 
-export function historyTempBox(){
-    switch (arguments.length) {
-        case 2: {
-          let historyBox = `${arguments[0]} ${arguments[1]}`; //backtick
-          historyNumber.innerHTML = historyBox ;
-          break;
-        }
-        case 4: {
-          let historyBox  = `${arguments[0]}  ${arguments[1]} ${arguments[2]} ${arguments[3]}`;
-          historyNumber.innerHTML = historyBox ;
-          break;
-        }
-      }
+export function historyTempBox() {
+ let historyBox = '';
+  for (let i = 0; i < historyBox.length; i++) {
+    historyBox += `${numberList[i]} ${signList[i]} `;
+  }
 }
