ea9ceb4617f498d1e966cc364ab7db1660977d64
Banasheh Sadeghi <haniehsadeghi2@outlook.com>
2023-02-14 09:50:21 +0330

    new mline div 500px hidde in show

--- Calculator/Theme/JS/Memory.js
+++ Calculator/Theme/JS/Memory.js
@@ -1,13 +1,24 @@
-
-export const mline = document.querySelectorAll('.mline'); //ms m+ m- mc
+let mlineM3 = '';
+const memoryContainer =document.getElementById('containerMemory');
+export const mlineBtn = document.querySelector('.mline'); //ms m+ m- mc
 const list = document.getElementById('history-msg'); //list history&&Memory
 // const memory = document.getElementById('memory-p2');//Memory
 const eventHandler = {};
-let messages = 'There\'s no history yet';
 
 let memoryList = [];
 export function addMemory(result) {
     memoryList.push(result);
 }
+
+mlineBtn.addEventListener('click', () => {
+    const visibility = memoryContainer.style.visibility;
+
+    if (visibility == 'hidden' || visibility == '') {
+        memoryContainer.style.visibility = 'visible';
+    }else{
+        memoryContainer.style.visibility = 'hidden';
+    }
+}); 
+
 
 
