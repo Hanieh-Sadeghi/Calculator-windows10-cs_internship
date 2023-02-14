b75117e2c591ea307496e26ac57ba70f30a4789e
Banasheh Sadeghi <haniehsadeghi2@outlook.com>
2023-02-14 14:36:06 +0330

    add target memory

--- Calculator/Theme/JS/Memory.js
+++ Calculator/Theme/JS/Memory.js
@@ -1,5 +1,5 @@
-let mlineM3 = '';
-const memoryContainer =document.getElementById('containerMemory');
+let numberValue = document.querySelector('#Zero');
+const memoryContainer = document.getElementById('containerMemory');
 export const mlineBtn = document.querySelector('.mline'); //ms m+ m- mc
 const list = document.getElementById('history-msg'); //list history&&Memory
 // const memory = document.getElementById('memory-p2');//Memory
@@ -9,16 +9,47 @@ let memoryList = [];
 export function addMemory(result) {
     memoryList.push(result);
 }
-
-mlineBtn.addEventListener('click', () => {
-    const visibility = memoryContainer.style.visibility;
-
-    if (visibility == 'hidden' || visibility == '') {
-        memoryContainer.style.visibility = 'visible';
-    }else{
+window.addEventListener('resize', resizeCalculateWindow);
+function resizeCalculateWindow() {
+    if (window.innerWidth > 500) {
         memoryContainer.style.visibility = 'hidden';
     }
-}); 
+}
+mlineBtn.addEventListener('click', (e) => {
+    if (window.innerWidth > 500) {
+        memoryContainer.style.visibility = 'hidden';
+    } else {
+        const visibility = memoryContainer.style.visibility;
+        if (e.target.getAttribute('value') == 'MC') {
+            memoryList = [];
+            list.innerHTML = '';
+        } else if (e.target.getAttribute('value') == 'M') {
+            {
+                if (visibility == 'hidden' || visibility == '') {
+                    memoryContainer.style.visibility = 'visible';
+                } else {
+                    memoryContainer.style.visibility = 'hidden';
 
-
-
+                }
+            }
+        }
+        else if (e.target.getAttribute('value') == 'M+') {
+            if (memoryList.length == 0) {
+                memoryList.push(Number(numberValue.innerHTML));
+            } else {
+                memoryList[memoryList.length - 1] += Number(numberValue.innerHTML);
+            }
+        }
+        else if (e.target.getAttribute('value') == 'M-') {
+            if (memoryList.length == 0) {
+                memoryList.push(Number(numberValue.innerHTML));
+            } else {
+                memoryList[memoryList.length - 1] -= Number(numberValue.innerHTML);
+            }
+        }
+        else if (e.target.getAttribute('value') == 'MS') {
+            memoryList.push(Number(numberValue.innerHTML));
+        }
+        console.log(memoryList);
+    }
+});
