28cc34885da80261938a3582759dca0709577a63
Banasheh Sadeghi <haniehsadeghi2@outlook.com>
2023-02-17 18:15:33 +0330

    displayTemoMemory() run

--- Theme/JS/Memory.js
+++ Theme/JS/Memory.js
@@ -1,10 +1,13 @@
+import * as appModule from './app.js';
 let numberValue = document.querySelector('#Zero');
 const memoryContainer = document.getElementById('containerMemory');
 const mlineBtn = document.querySelector('.mline'); //ms m+ m- mc
 const list = document.getElementById('history-msg'); //list history&&Memory
 // const memory = document.getElementById('memory-p2');//Memory
-const eventHandler = {};
-const listMemory = document.getElementById('memory-msg'); //Memory
+// const eventHandler = {};
+const listMemory = document.getElementById('memoryLog'); //Memory
+
+
 
 let memoryList = [];
 export function addMemory(result) {
@@ -20,10 +23,12 @@ mlineBtn.addEventListener('click', (e) => {
     if (window.innerWidth > 500) {
         memoryContainer.style.visibility = 'hidden';
     }
+
     const visibility = memoryContainer.style.visibility;
     if (e.target.getAttribute('value') == 'MC') {
         memoryList = [];
         list.innerHTML = '';
+        listMemory.innerHTML = `<p id="memoryLog" class="nohistory "> There's no memory yet </p>`
     } else if (e.target.getAttribute('value') == 'M') {
         {
             if (visibility == 'hidden' || visibility == '') {
@@ -40,6 +45,7 @@ mlineBtn.addEventListener('click', (e) => {
         } else {
             memoryList[memoryList.length - 1] += Number(numberValue.innerHTML);
         }
+        displayTempMemory();
     }
     else if (e.target.getAttribute('value') == 'M-') {
         if (memoryList.length == 0) {
@@ -47,36 +53,45 @@ mlineBtn.addEventListener('click', (e) => {
         } else {
             memoryList[memoryList.length - 1] -= Number(numberValue.innerHTML);
         }
+        displayTempMemory();
     }
     else if (e.target.getAttribute('value') == 'MS') {
         memoryList.push(Number(numberValue.innerHTML));
-        displayTemoMemory(269013399999)
+        displayTempMemory();
     }
     else if (e.target.getAttribute('value') == 'MR') {
         if (memoryList.length == 0) {
             numberValue.innerHTML = 0;
+        } else {
+            numberValue.innerHTML = memoryList[memoryList.length - 1];
         }
-
+        appModule.clearTempBox();
     }
-
     console.log(memoryList);
 });
 
 
 
-export function displayTemoMemory(number) {
-    console.log('navid')
-    let memoryList = ' ';
-    for (let i = 0; i < memoryList.length; i++);
-    listMemory.innerHTML = `<p id="memoryLog" class="nohistory ">There's no history yet</p>
-    <div class ="memoryNumber"> 
-        <div class = "NumberM"> ${number} </div>
-            <div class = "spnMemory">
-            <spn>mc </spn>
-            <spn>m+ </spn>
-            <spn>m- </spn>
-            </div>
-     </div>
-    <button id="material-symbols-outlined" class="material-symbols-outlined"> delete</button>`
-
+export function displayTempMemory() {
+    listMemory.innerHTML = ' ';
+    for (let i = 0; i < memoryList.length; i++) {
+        listMemory.innerHTML +=
+            ` <div class ="memoryNumber"> 
+                <div class = "NumberM"> ${memoryList[i]} </div>
+                <div class = "spnMemory">
+                <spn id="${i}" value="MC">mc </spn>
+                <spn id="${i}" value="M+">m+ </spn>
+                <spn id="${i}" value="M-">m- </spn>
+                </div>
+         </div>`
+    }
+    debugger;
+    let memoryItemBtn = document.querySelectorAll('.spnMemory'); // spn mc m+ m-
+    memoryItemBtn.addEventListener('click', (e) => {
+        let index = Number(e.target.getAttribute('id'));
+        let operator = e.target.getAttribute('value');
+        if (operator == 'M+') {
+            memoryList[index] += Number(numberValue.innerHTML);
+        }
+    });
 }
